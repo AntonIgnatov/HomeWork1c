18c1df75c152a1214396e4634608653820cf0bfb
AntonIgnatov <venzor@bigmir.net>
2017-06-13 11:15:32 +0300

    Update Vector3d.java

--- src/net/bigmir/venzor/Vector3d.java
+++ src/net/bigmir/venzor/Vector3d.java
@@ -63,7 +63,7 @@ public class Vector3d {
 
 	@Override
 	public String toString() {
-		return "{" + x + "; " + y + "; " + z + "}";
+		return "{" + String.format("%.1f.", x) + "; " + String.format("%.1f.", y) + "; " + String.format("%.1f.", z) + "}";
 	}
 
 }
