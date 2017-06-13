fe4a442dd18d40205fde12265a68aee3fea656d8
AntonIgnatov <venzor@bigmir.net>
2017-06-13 11:15:56 +0300

    Update Main.java

--- src/net/bigmir/venzor/Main.java
+++ src/net/bigmir/venzor/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		System.out.println("Sum of vectors a and b: c"+s.toString());
 		double scal=Vector3d.scalar(a, b);
 		System.out.println("Scalar multiplication of vectors a and b =  "+scal);
-		Vector3d v=Vector3d.vectSum(a, b);
+		Vector3d v = Vector3d.vectProizv(a, b);
 		System.out.println("Vector multiplication of vectors a and b: c"+v.toString());
 		
 		
